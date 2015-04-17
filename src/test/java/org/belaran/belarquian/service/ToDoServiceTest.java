254e5234d2a95bfc8606b0f2526f0f19b821f35d
Romain PELISSE <belaran@gmail.com>
2015-04-17 18:54:38 +0200

    Add first test with arquillian

---
+++ src/test/java/org/belaran/belarquian/service/ToDoServiceTest.java
@@ -0,0 +1,33 @@
+package org.belaran.belarquian.service;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ToDoServiceTest {
+	
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create(JavaArchive.class).addClass(TodoService.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Inject 
+	private TodoService todoService;
+
+	@Test
+	public void test() {
+		todoService.addToDo(new ToDoItem(1L, "first item"));
+		assertEquals(1, todoService.getTodos().size());
+	}
+
+}
