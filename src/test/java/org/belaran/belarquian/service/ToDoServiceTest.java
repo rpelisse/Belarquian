0020a938f65fd2996b8a26e8513fc727a0f847fa
Romain PELISSE <belaran@gmail.com>
2015-04-21 16:37:55 +0200

    Add test for WS

--- src/test/java/org/belaran/belarquian/service/ToDoServiceTest.java
+++ src/test/java/org/belaran/belarquian/service/ToDoServiceTest.java
@@ -1,9 +1,16 @@
 package org.belaran.belarquian.service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ZipImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -12,19 +19,43 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class ToDoServiceTest {
-	
+
 	@Deployment
 	public static WebArchive createDeployment() {
-	    return ShrinkWrap.create(ZipImporter.class, "Belarquian.war.war").importFrom(new File("target/Belarquian.war"))
-	            .as(WebArchive.class);
+		return ShrinkWrap.create(ZipImporter.class, "Belarquian.war.war")
+				.importFrom(new File("target/Belarquian.war"))
+				.as(WebArchive.class);
 	}
-	
-//	@Inject 
-//	private TodoService todoService;
 
 	@Test
-	public void test() {
-//		todoService.addToDo(new ToDoItem(1L, "first item"));
-	//	assertEquals(1, todoService.getTodos().size());
+	@RunAsClient
+	public void checkServiceStatus(@ArquillianResource URL baseURL)
+			throws IOException {
+		printInputStream(baseURL.toString() + "rest/test");		
 	}
+
+	private static void printInputStream(String urlAsString) throws IOException {		
+		System.out.println("Invoking URL:" + urlAsString);
+		BufferedReader in = new BufferedReader(new InputStreamReader(new URL(urlAsString).openStream()));
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+		    System.out.println(inputLine);
+		in.close();
+	}
+	
+	@Test
+	@RunAsClient
+	public void listItems(@ArquillianResource URL baseURL)
+			throws IOException {
+		printInputStream(baseURL.toString() + "rest/add/1/a-value");
+		printInputStream(baseURL.toString() + "rest/list");
+	}
+
+	@Test
+	@RunAsClient
+	public void addOneItemTest(@ArquillianResource URL baseURL)
+			throws IOException {
+		printInputStream(baseURL.toString() + "rest/add/1/a-value");
+	}
+
 }
